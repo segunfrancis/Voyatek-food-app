044df80a5fce7784f5ec3400ce4e4430af8880de
Segun Francis <francis4dblues@gmail.com>
2025-02-25 19:03:13 +0100

    Initial project setup
    * Add dependencies
    * Add Fonts

---
+++ app/src/main/java/com/segunfrancis/food_app_assessment/app_navigation/NavDestinations.kt
@@ -0,0 +1,20 @@
+package com.segunfrancis.food_app_assessment.app_navigation
+
+import kotlinx.serialization.Serializable
+
+sealed class NavDestinations {
+    @Serializable
+    data object Home : NavDestinations()
+
+    @Serializable
+    data object Generator : NavDestinations()
+
+    @Serializable
+    data object Add : NavDestinations()
+
+    @Serializable
+    data object Favourite : NavDestinations()
+
+    @Serializable
+    data object Planner : NavDestinations()
+}
